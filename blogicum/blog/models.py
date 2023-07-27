09d99bfb5e1738f1a90ea201c261a56e669ba5a4
kran313 <96043976+kran313@users.noreply.github.com>
2023-07-27 18:34:11 +0300

    Update models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -26,8 +26,8 @@ class Category(BaseModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text=('Идентификатор страницы для URL; ' +
-        'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+        help_text=('Идентификатор страницы для URL; '
+                   + 'разрешены символы латиницы, цифры, дефис и подчёркивание.')
     )
 
     class Meta:
@@ -54,8 +54,8 @@ class Post(BaseModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=('Если установить дату и время в будущем — ' +
-        'можно делать отложенные публикации.')
+        help_text=('Если установить дату и время в будущем — '
+                   + 'можно делать отложенные публикации.')
     )
     author = models.ForeignKey(
         User,
